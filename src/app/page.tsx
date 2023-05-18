4fd52d133fe61557144df220f43d4c488be98869
Arthur <arthur.moreira@itec.ufpa.br>
2023-05-17 23:05:39 -0300

    temp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
-  return <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
-    <h1 className="text-4xl font-bold">
-      Sua cápsula do tempo
-    </h1>
-  </div>
+  return <main className=" grid min-h-screen grid-cols-2">
+    <div></div>
+
+    {/*Right side*/}
+    <div className="flex flex-col p-16">
+      <div className="flex flex-1 items-center justify-center">
+        <p className="text-center leading-relaxed w-[360px]">
+          Você ainda não registrou nenhuma memória, comece a{' '}
+          <a href="/" className="">crirar agora!</a>
+        </p>
+      </div>
+    </div>
+  </main>
 }
